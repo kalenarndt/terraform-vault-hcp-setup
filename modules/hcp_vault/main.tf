07d8487acdb7ff7b05bf98cba5ec91ffdf4294cf
kalenarndt <kalenarndt@gmail.com>
2021-08-13 21:57:01 +0000

    Consul cluster support

--- modules/hcp_vault/main.tf
+++ modules/hcp_vault/main.tf
@@ -1,7 +1,8 @@
 // creates a route for HVN to aws via the vpc
 resource "hcp_hvn_route" "hvn_vpc_route" {
-  count            = var.vpc_peering ? 1 : 0
-  hvn_link         = hcp_hvn.hcp_vault_hvn.self_link
+  count = var.vpc_peering ? 1 : 0
+
+  hvn_link         = hcp_hvn.hcp_hvn.self_link
   hvn_route_id     = var.hvn_route_id
   destination_cidr = var.vpc_cidr
   target_link      = hcp_aws_network_peering.hvn_aws_peer[0].self_link
@@ -9,8 +10,9 @@ resource "hcp_hvn_route" "hvn_vpc_route" {
 
 // creates a peering request with aws
 resource "hcp_aws_network_peering" "hvn_aws_peer" {
-  count           = var.vpc_peering ? 1 : 0
-  hvn_id          = hcp_hvn.hcp_vault_hvn.hvn_id
+  count = var.vpc_peering ? 1 : 0
+
+  hvn_id          = hcp_hvn.hcp_hvn.hvn_id
   peering_id      = var.hvn_peering_id
   peer_vpc_id     = var.vpc_id
   peer_account_id = var.vpc_owner_id
@@ -18,7 +20,7 @@ resource "hcp_aws_network_peering" "hvn_aws_peer" {
 }
 
 // creates the hvn network resource
-resource "hcp_hvn" "hcp_vault_hvn" {
+resource "hcp_hvn" "hcp_hvn" {
   hvn_id         = var.hvn_id
   cloud_provider = var.cloud_provider
   region         = var.region
@@ -27,17 +29,19 @@ resource "hcp_hvn" "hcp_vault_hvn" {
 
 // creates the vault cluster on the hvn network resource
 resource "hcp_vault_cluster" "vault_cluster" {
-  count             = var.create_vault_cluster ? 1 : 0
-  hvn_id            = hcp_hvn.hcp_vault_hvn.hvn_id
-  cluster_id        = var.hcp_cluster_id
-  tier              = var.hcp_vault_tier
-  public_endpoint   = var.hcp_public_endpoint
+  count = var.create_vault_cluster ? 1 : 0
+
+  hvn_id            = hcp_hvn.hcp_hvn.hvn_id
+  cluster_id        = var.vault_cluster_name
+  tier              = var.vault_tier
+  public_endpoint   = var.vault_public_endpoint
   min_vault_version = var.min_vault_version
 }
 
 // accept the peering request between hvn and aws
 resource "aws_vpc_peering_connection_accepter" "hvn_aws_vpc_accept" {
-  count                     = var.vpc_peering ? 1 : 0
+  count = var.vpc_peering ? 1 : 0
+
   vpc_peering_connection_id = hcp_aws_network_peering.hvn_aws_peer[0].provider_peering_id
   auto_accept               = true
   tags = {
@@ -47,8 +51,9 @@ resource "aws_vpc_peering_connection_accepter" "hvn_aws_vpc_accept" {
 
 // creates an attachment to the aws transit gateway from hvn
 resource "hcp_aws_transit_gateway_attachment" "hvn_transit_gw" {
-  count                         = var.transit_gw ? 1 : 0
-  hvn_id                        = hcp_hvn.hcp_vault_hvn.hvn_id
+  count = var.transit_gw ? 1 : 0
+
+  hvn_id                        = hcp_hvn.hcp_hvn.hvn_id
   transit_gateway_attachment_id = var.transit_gw_attachment_id
   transit_gateway_id            = var.transit_gw_id
   resource_share_arn            = var.resource_share_arn
@@ -56,23 +61,39 @@ resource "hcp_aws_transit_gateway_attachment" "hvn_transit_gw" {
 
 // accept the hvn attachment to the transit gateway
 resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "hvn_aws_tgw_accept" {
-  count                         = var.transit_gw ? 1 : 0
+  count = var.transit_gw ? 1 : 0
+
   transit_gateway_attachment_id = hcp_aws_transit_gateway_attachment.hvn_transit_gw[0].provider_transit_gateway_attachment_id
 }
 
 // associates the hcp provider id with the resource_share arn in aws
 resource "aws_ram_principal_association" "hcp_aws_ram" {
-  count              = var.transit_gw ? 1 : 0
+  count = var.transit_gw ? 1 : 0
+
   resource_share_arn = var.resource_share_arn
-  principal          = hcp_hvn.hcp_vault_hvn.provider_account_id
+  principal          = hcp_hvn.hcp_hvn.provider_account_id
 }
 
 // creates a route from hvn to aws via the transit gateway
 resource "hcp_hvn_route" "hvn_tgw_route" {
-  count            = var.transit_gw ? 1 : 0
-  hvn_link         = hcp_hvn.hcp_vault_hvn.self_link
+  count = var.transit_gw ? 1 : 0
+
+  hvn_link         = hcp_hvn.hcp_hvn.self_link
   hvn_route_id     = var.hvn_route_id
   destination_cidr = var.vpc_cidr
   target_link      = hcp_aws_transit_gateway_attachment.hvn_transit_gw[0].self_link
 }
 
+// creates the hcp consul cluster
+resource "hcp_consul_cluster" "consul_cluster" {
+  count = var.create_consul_cluster ? 1 : 0
+
+  cluster_id         = var.consul_cluster_name
+  hvn_id             = hcp_hvn.hcp_hvn.hvn_id
+  tier               = var.consul_tier
+  size               = var.consul_size
+  public_endpoint    = var.consul_public_endpoint
+  datacenter         = var.consul_datacenter != null ? var.consul_datacenter : var.consul_cluster_name
+  min_consul_version = var.min_consul_version
+  connect_enabled    = var.connect_enabled
+}
--- modules/hcp_vault/variables.tf
+++ modules/hcp_vault/variables.tf
@@ -1,3 +1,109 @@
+variable "create_vault_cluster" {
+  description = "Flag to create a Vault cluster"
+  type        = bool
+  default     = false
+}
+
+variable "create_consul_cluster" {
+  description = "Flag to create an HCP Consul cluster"
+  type        = bool
+  default     = false
+}
+
+
+variable "vault_tier" {
+  description = "Tier to provision in HCP Vault - dev, standard_small, standard_medium, standard_large"
+  type        = string
+  default     = "dev"
+  validation {
+    condition     = var.vault_tier != "dev" || var.vault_tier != "standard_small" || var.vault_tier != "standard_medium" || var.vault_tier != "standard_large"
+    error_message = "The variable vault_tier must be \"dev\", \"standard_small\", \"standard_medium\", or \"standard_large\"."
+  }
+}
+
+variable "vault_cluster_name" {
+  description = "The name (id) of the HCP Vault cluster."
+  type        = string
+  default     = "hcp-vault-cluster"
+}
+
+variable "vault_public_endpoint" {
+  description = "Exposes the cluster to the internet. Defaults to false"
+  type        = bool
+  default     = false
+}
+
+variable "min_vault_version" {
+  description = "Minimum Vault version to use when creating the cluster. If null, defaults to HCP recommended version"
+  type        = string
+  default     = null
+}
+
+##### Consul
+variable "consul_size" {
+  description = "Size of the HCP Consul nodes - x_small, small, medium, large - x_small only works with development tier"
+  type        = string
+  default     = "x_small"
+  validation {
+    condition     = var.consul_size != "x_small" || var.consul_size != "small" || var.consul_size != "medium" || var.consul_size != "large"
+    error_message = "The variable consul_size must be \"x_small\", \"small\", \"medium\", or \"large\"."
+  }
+}
+
+variable "consul_tier" {
+  description = "Tier of the HCP Consul cluster - development, standard, plus - x_small only works with development tier"
+  type        = string
+  default     = "development"
+  validation {
+    condition     = var.consul_tier != "development" || var.consul_tier != "standard" || var.consul_tier != "plus"
+    error_message = "The variable consul_size must be \"development\", \"standard\", or \"plus\"."
+  }
+}
+
+variable "consul_public_endpoint" {
+  description = "Exposes the Consul cluster to the internet. Defaults to false"
+  type        = bool
+  default     = false
+}
+
+variable "min_consul_version" {
+  description = "Minimum Consul version to use when creating the cluster. If null, defaults to HCP recommended version"
+  type        = string
+  default     = null
+}
+
+variable "connect_enabled" {
+  description = "Denotes the Consul connect feature should be enabled. Defaults to true"
+  type        = bool
+  default     = true
+}
+
+variable "consul_cluster_name" {
+  description = "The name (id) of the HCP Consul cluster"
+  type        = string
+  default     = "hcp-consul-cluster"
+}
+
+variable "consul_datacenter" {
+  description = "The Consul datacenter name. If set to null the datacenter will be set to the Consul cluster name. Defaults to null"
+  type        = string
+  default     = null
+}
+
+#### End Consul ####
+
+variable "vpc_peering" {
+  description = "Flag to enable vpc peering with HCP and AWS"
+  type        = bool
+  default     = true
+}
+
+variable "transit_gw" {
+  description = "Flag to use an AWS transit gateway"
+  type        = bool
+  default     = false
+}
+
 variable "vpc_id" {
   description = "Peer ID from the AWS peering VPC"
   type        = string
@@ -8,12 +114,6 @@ variable "vpc_owner_id" {
   description = "Peer account ID from AWS"
   type        = string
   default     = ""
-}
-
-variable "vpc_peering" {
-  description = "Flag to enable vpc peering with HCP and AWS"
-  type        = bool
-  default     = true
 }
 
 variable "vpc_cidr" {
@@ -31,22 +131,6 @@ variable "hcp_cidr_block" {
   description = "CIDR block for the HVN VPC"
   type        = string
   default     = "172.25.16.0/20"
-}
-
-variable "hcp_vault_tier" {
-  description = "Tier to provision in HCP Vault - dev, standard_small, standard_medium, standard_large"
-  type        = string
-  default     = "dev"
-  validation {
-    condition     = var.hcp_vault_tier != "dev" || var.hcp_vault_tier != "standard_small" || var.hcp_vault_tier != "standard_medium" || var.hcp_vault_tier != "standard_large"
-    error_message = "The variable hcp_vault_tier must be \"dev\", \"standard_small\", \"standard_medium\", or \"standard_large\"."
-  }
-}
-
-variable "create_vault_cluster" {
-  description = "Flag to create a vault cluster"
-  type        = bool
-  default     = true
 }
 
 variable "hvn_route_id" {
@@ -67,23 +151,14 @@ variable "cloud_provider" {
   default     = "aws"
 }
 
-variable "hcp_public_endpoint" {
-  description = "Exposes the cluster to the internet. Defaults to false"
-  type        = bool
-  default     = false
-}
-
 variable "hvn_id" {
   description = "The ID of the HCP HVN."
   type        = string
   default     = "hcp-vault-hvn"
 }
 
-variable "hcp_cluster_id" {
-  description = "The ID of the HCP Vault cluster."
-  type        = string
-  default     = "hcp-vault-cluster"
-}
+
+
 
 variable "hvn_peering_id" {
   description = "The ID of the HCP peering connection."
@@ -91,11 +166,7 @@ variable "hvn_peering_id" {
   default     = "hcp-hvn-peering"
 }
 
-variable "min_vault_version" {
-  description = "Minimum Vault version to use when creating cluster. If null, defaults to HCP recommended version"
-  type        = string
-  default     = null
-}
+
 
 variable "transit_gw_attachment_id" {
   description = "Name of the transit gateway attachment in HVN"
@@ -103,11 +174,6 @@ variable "transit_gw_attachment_id" {
   default     = "hcp-hvn-transit-gw"
 }
 
-variable "transit_gw" {
-  description = "Flag to use an aws transit gateway"
-  type        = bool
-  default     = false
-}
 
 variable "transit_gw_id" {
   description = "ID of the transit gateway that exists in AWS that HCP will attach to"
